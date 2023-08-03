5381ae13267d0d05a078d4e3816f756e0ea479ac
gerogelin <gerogelin@oringnet.com>
2023-08-03 13:52:26 +0800

    add user dto

---
+++ src/users/dto/user-login.dto.ts
@@ -0,0 +1,13 @@
+import { 
+  IsNotEmpty 
+} from "class-validator";
+
+
+export class LoginUserDto {
+
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+}
---
+++ src/users/dto/user.create.dto.ts
@@ -0,0 +1,19 @@
+import { 
+  IsEmail, 
+  IsNotEmpty 
+} from "class-validator";
+
+
+
+export class CreateUserDto {
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
