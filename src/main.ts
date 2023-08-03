10766db6e4f49a7d367c3c577ff28dd0988f1a3d
gerogelin <gerogelin@oringnet.com>
2023-08-03 13:08:12 +0800

    add swagger in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+
+  // add swagger.
+  const config = new DocumentBuilder()
+    .setTitle('nestjs-demo-example')
+    .setDescription('API of nestjs-demo-example')
+    .setVersion('1.0')
+    // .addTag('nestjs-demo-example')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
